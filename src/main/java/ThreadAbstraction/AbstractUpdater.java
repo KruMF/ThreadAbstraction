69d10f0c4335832c72dbda3282c819f5a7e5530e
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-05-06 14:27:40 +0300

    abstractUpdater partial javadoc

--- src/main/java/ThreadAbstraction/AbstractUpdater.java
+++ src/main/java/ThreadAbstraction/AbstractUpdater.java
@@ -6,23 +6,42 @@ import DelayCalculator.DelayOptions;
 //TODO: add javadoc
 public abstract class AbstractUpdater extends AbstractThread {
     private boolean running;
+
+    /**
+     * A public-access DelayCalculator object for handling thread delay.
+     * Use this to set/reset preferences.
+     */
     public DelayCalculator delayCalculator;
 
+    /**
+     * Creates an abstract updater with default delay options.
+     */
     public AbstractUpdater() {
         running = false;
         delayCalculator = new DelayCalculator();
     }
 
+    /**
+     * Creates an abstract updater with specified delay in milliseconds.
+     *
+     * @param delay Milliseconds.
+     */
     public AbstractUpdater(long delay) {
         running = false;
         delayCalculator = new DelayCalculator(delay);
     }
 
+    /**
+     * Creates an abstract updater with specified delay options.
+     *
+     * @param delayOptions Delay options.
+     */
     public AbstractUpdater(DelayOptions delayOptions) {
         running = false;
         delayCalculator = new DelayCalculator(delayOptions);
     }
 
+    //TODO: add javadoc
     @SuppressWarnings("BusyWait")
     @Override
     public final void run() {
@@ -39,9 +58,11 @@ public abstract class AbstractUpdater extends AbstractThread {
         finish();
     }
 
+    //TODO: add javadoc
     //override this to check for cycle breaking
     public void endCheck() {}
 
+    //TODO: add javadoc
     //call this to stop
     public final void end() {
         running = false;
