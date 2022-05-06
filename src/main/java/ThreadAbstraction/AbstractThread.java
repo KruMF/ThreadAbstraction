0807270dfd94af5fe35aa73f7b3de0c95f1f42ac
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-05-06 14:59:37 +0300

    0.2
    javadoc & readme

--- src/main/java/ThreadAbstraction/AbstractThread.java
+++ src/main/java/ThreadAbstraction/AbstractThread.java
@@ -1,17 +1,31 @@
 package ThreadAbstraction;
 
-//TODO: add javadoc
+/**
+ * An abstract thread that runs only once.
+ * Use start() to start the thread.
+ * Has to be re-created upon every re-use.
+ */
 public abstract class AbstractThread extends Thread implements Runnable {
 
+    /**
+     * Happens when thread runs.
+     * Do not override this (see update() for custom actions).
+     */
     @Override
     public void run() {
         update();
         finish();
     }
 
-    //override this for thread actions
+    /**
+     * Gets called when thread runs.
+     * Override this method for custom actions.
+     */
     public abstract void update();
 
-    //happens upon finishing thread; can be overriden
+    /**
+     * Happens upon finishing thread.
+     * Can be overriden for custom ending.
+     */
     public void finish() {}
 }
